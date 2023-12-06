fb16774632e0e1ca28c25ac78359479fa48a3de4
kapil <duckatgit@gmail.com>
2023-12-06 13:06:12 +0530

    signin and signup changes

--- api/controllers/uploadProjectController.js
+++ api/controllers/uploadProjectController.js
@@ -10,6 +10,7 @@ const { google } = require("googleapis");
 const gmail = google.gmail("v1");
 const axios = require("axios");
 const { logErrorToMongoDB } = require("../services/logError");
+const User = require("../models/usersModel");
 
 exports.uploadJson = async function (req, res) {
   try {
@@ -68,7 +69,6 @@ exports.sendSignatureTemplateDemo = async function (req, res) {
   const CLIENT_ID = process.env.CLIENT_ID;
   const CLIENT_SECRET = process.env.CLIENT_SECRET;
   const REDIRECT_URI = process.env.REDIRECT_URI;
-  const TOKEN_URL = process.env.TOKEN_URL;
 
   // Initialize OAuth2 client
   const oAuth2Client = new google.auth.OAuth2(
@@ -82,7 +82,8 @@ exports.sendSignatureTemplateDemo = async function (req, res) {
       access_type: "offline",
       scope: [
         "https://www.googleapis.com/auth/gmail.settings.basic",
-        "https://www.googleapis.com/auth/userinfo.email", // Include OpenID Connect scope
+        "https://www.googleapis.com/auth/userinfo.email",
+        "https://www.googleapis.com/auth/userinfo.profile",
       ],
     });
 
@@ -93,6 +94,60 @@ exports.sendSignatureTemplateDemo = async function (req, res) {
     logErrorToMongoDB("sendSignatureTemplateDemo", error);
   }
 };
+
+exports.googleAuthCallback = async function (req, res) {
+  try {
+    const { code } = req.body;
+    const CLIENT_ID = process.env.CLIENT_ID;
+    const CLIENT_SECRET = process.env.CLIENT_SECRET;
+    const REDIRECT_URI = process.env.REDIRECT_URI;
+    const oAuth2Client = new google.auth.OAuth2(
+      CLIENT_ID,
+      CLIENT_SECRET,
+      REDIRECT_URI
+    );
+
+    const { tokens } = await oAuth2Client.getToken(code);
+    const accessToken = tokens.access_token;
+
+    // Get user information using the access token
+    const userInfo = await getUserInfo(accessToken);
+
+    // Extract the necessary information like email and name
+    const email = userInfo.email;
+    const name = userInfo.name;
+    let existingUser = await User.findOne({ email });
+
+    // If the user doesn't exist, create a new user
+    if (!existingUser) {
+      let data = {
+        firstName: name.split(" ")[0],
+        lastName: name.split(" ")[1],
+        gender: "Unknown",
+        email: email,
+        password: "password",
+      };
+
+      var newUser = new User(data); // Use 'User' instead of 'users'
+      const result = await newUser.save();
+    }
+
+    res.send({ accessToken, email, name });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch user information" });
+    logErrorToMongoDB("googleAuthCallback", error);
+  }
+};
+
+async function getUserInfo(accessToken) {
+  const url = "https://www.googleapis.com/oauth2/v2/userinfo";
+  const response = await axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+  return response.data;
+}
 
 // Add a new route for handling the callback after the user grants permission
 exports.signatureCallback = async function (req, res) {
@@ -113,14 +168,15 @@ exports.signatureCallback = async function (req, res) {
       client_secret: CLIENT_SECRET,
       redirect_uri: REDIRECT_URI,
       grant_type: "authorization_code",
-      access_type: "offline",
     });
 
     const accessToken = tokenResponse.data.access_token;
     const refreshToken = tokenResponse.data.refresh_token;
     const idToken = tokenResponse.data.id_token; // Retrieve ID token from the response
 
-    const userEmail = await getEmailFromIdToken(idToken);
+    const data = await getEmailFromIdToken(idToken);
+    const userEmail = data.email;
+    const userName = data.name;
     // Update Gmail signature using the obtained tokens
     await updateSignature(accessToken, refreshToken, structure, userEmail);
 
@@ -128,10 +184,11 @@ exports.signatureCallback = async function (req, res) {
     res.send({
       accessToken,
       refreshToken,
+      userName,
       idToken, // Send ID token to the client
     });
   } catch (error) {
-    res.status(500).send("Error exchanging authorization code for tokens");
+    res.status(500).send(error);
     logErrorToMongoDB("signatureCallback", error);
   }
 };
@@ -168,8 +225,6 @@ async function updateSignature(
         signature: structure,
       },
     });
-
-    console.log("Signature updated:", response.data);
   } catch (error) {
     console.error("Error updating signature:", error.message);
     logErrorToMongoDB("updateSignature", error);
